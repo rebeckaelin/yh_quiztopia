50ccd96e4a510ea8d35db4f41ddc84bd3f192345
rebeckaelin <rebeckaelin@hotmail.com>
2024-09-30 15:55:06 +0200

    postquiz and postquestions all done

---
+++ services/api/getQuizes.js
@@ -0,0 +1,21 @@
+const {db} = require("../data/db")
+const {sendResponse, sendError} = require("../utils/responses");
+
+exports.handler = async(event) => {
+
+    try {
+
+        const result = db.get({
+            TableName: "quizes",
+            KeyConditionExpression: "quizType = :type",
+            ExpressionAttributeValues: {
+                ":type": "quiz"
+            } 
+        })
+
+        return sendResponse(200, "Success", result)
+        
+    } catch (error) {
+        sendError(500, "Error fetching quizes")
+    }
+}
