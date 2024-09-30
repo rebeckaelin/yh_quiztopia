ca0d550a5264acad8b9a13521a20abf9d03b88d9
rebeckaelin <rebeckaelin@hotmail.com>
2024-09-30 16:43:36 +0200

    done with deletequiz

---
+++ services/api/deleteQuiz.js
@@ -0,0 +1,47 @@
+
+const {db} = require("../data/db")
+const {sendResponse, sendError} = require("../utils/responses");
+const { validateToken } = require("../../middleware/validateToken");
+const middy = require("@middy/core");
+
+const handler = middy().use(validateToken()).handler(async (event) => {
+    const quizId = event.pathParameters.quizId
+    const loggedInUser = event.userId
+
+    if(!quizId){
+        return sendError(400, "Quiz id is required in URL")
+    }
+
+    try {
+        const existingQuiz = await db.get({
+            TableName: "quizes",
+            Key: {quizType: "quiz", quizId: quizId}
+        })
+
+        if (!existingQuiz.Item) {
+            return sendError(404, "Quiz not found..")
+        }
+        if (existingQuiz.Item.userId !== loggedInUser) {
+            return sendError(403, "You do not have permission to delete this quiz.")
+        }
+
+        const deletedQuiz = await db.delete({
+            TableName: "quizes",
+            Key: {
+                quizType: "quiz",
+                quizId: quizId
+            },
+            ReturnValues: "ALL_OLD"
+        })
+
+        return sendResponse(200, "Quiz successfully deleted", deletedQuiz.Attributes )
+        
+    } catch (error) {
+        console.error("Error deleting quiz:", error);
+        return sendError(500, "Error deleting quiz.");
+    }
+
+
+})
+
+module.exports = {handler}
