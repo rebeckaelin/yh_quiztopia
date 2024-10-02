b8eb3e42a49f16ed8898b145cd16100f95ba824b
rebeckaelin <rebeckaelin@hotmail.com>
2024-10-02 12:58:09 +0200

    added and implemented new function

--- services/auth/login.js
+++ services/auth/login.js
@@ -1,31 +1,34 @@
-
-const {sendResponse, sendError} = require("../utils/responses");
+const { sendResponse, sendError } = require("../utils/responses");
 const { comparePassword } = require("../utils/comparePassword");
 const { getUser } = require("../utils/getUserByUsername");
 const { generateToken } = require("../auth/generateToken");
+const { checkUserData } = require("../utils/checkUserData");
 
 exports.handler = async (event) => {
-    const {username, password} = JSON.parse(event.body)
+  const { username, password } = JSON.parse(event.body);
 
-    if (!username || username.trim() === "" || !password || password.trim() === "") {
-        return sendError(400, "Username and password are required.");
+  try {
+    await checkUserData(username, password);
+  } catch (error) {
+    return sendError(400, error.message);
+  }
+
+  try {
+    const user = await getUser(username);
+
+    if (!user) {
+      return sendError(401, "Wrong username or password");
     }
 
-    try {
-        const user = await getUser(username)
-        
-        if(!user) {
-            return sendError(401, "Wrong username or password")
-        }
-        
-        const correctPassword = await comparePassword(password, user)
-        if (!correctPassword) {return sendError(401, "Wrong username or password")}
-
-        const token = generateToken(user)
-        
-        return sendResponse(200, "Login successful", {token: token})
-        
-    } catch (error) {
-        return sendError(500, "Internal server error")
+    const correctPassword = await comparePassword(password, user);
+    if (!correctPassword) {
+      return sendError(401, "Wrong username or password");
     }
-}
+
+    const token = generateToken(user);
+
+    return sendResponse(200, "Login successful", { token: token });
+  } catch (error) {
+    return sendError(500, "Internal server error");
+  }
+};
--- services/auth/signup.js
+++ services/auth/signup.js
@@ -1,53 +1,57 @@
-const {db} = require("../data/db")
-const {v4: uuid} = require("uuid")
-const {sendResponse, sendError} = require("../utils/responses");
+const { db } = require("../data/db");
+const { v4: uuid } = require("uuid");
+const { sendResponse, sendError } = require("../utils/responses");
 const { hashPassword } = require("../utils/hashPassword");
 const { getUser } = require("../utils/getUserByUsername");
-
+const { checkUserData } = require("../utils/checkUserData");
 
 exports.handler = async (event) => {
-    const {username, password} = JSON.parse(event.body)
+  const { username, password } = JSON.parse(event.body);
 
-    if (!username || username.trim() === "" || !password || password.trim() === "") {
-        return sendError(400, "Username and password are required.");
+  // if (
+  //   !username ||
+  //   username.trim() === "" ||
+  //   !password ||
+  //   password.trim() === ""
+  // ) {
+  //   return sendError(400, "Username and password are required.");
+  // }
+  try {
+    await checkUserData(username, password);
+  } catch (error) {
+    return sendError(400, error.message);
+  }
+
+  const usernameRegex = /^[a-zA-Z]+$/; // Endast bokstäver
+  if (!usernameRegex.test(username)) {
+    return sendError(400, "Username can only contain letters (A-Z, a-z).");
+  }
+
+  let hashedPassword;
+
+  try {
+    hashedPassword = await hashPassword(password);
+  } catch (error) {
+    return sendError(500, "Error while hashing password");
+  }
+
+  try {
+    const userExists = await getUser(username);
+    if (userExists) {
+      return sendError(409, "Username already exists");
     }
 
-    const usernameRegex = /^[a-zA-Z]+$/; // Endast bokstäver
-    if (!usernameRegex.test(username)) {
-        return sendError(400, "Username can only contain letters (A-Z, a-z).");
-    }
-
-    let hashedPassword
-    try {
-        hashedPassword =  await hashPassword(password)
-        
-    } catch (error) {
-        return sendError(500, "Error while hashing password" + error.message)
-    }
-
-    try {
-
-        const userExists = await getUser(username)
-        if(userExists) {
-            return {
-                statusCode: 409,
-                body: JSON.stringify({message: "Username already exists"})
-            }
-        }
-
-        const newUser = {
-            userId: uuid(),
-            username: username,
-            password: hashedPassword
-        }
-        await db.put({
-            TableName: "users",
-            Item: newUser          
-        })
-        return sendResponse(201, "New user created", newUser)
-        
-    } catch (error) {
-        return sendError(500, error.message) 
-    }
-
-}
+    const newUser = {
+      userId: uuid(),
+      username: username,
+      password: hashedPassword,
+    };
+    await db.put({
+      TableName: "users",
+      Item: newUser,
+    });
+    return sendResponse(201, "New user created", newUser);
+  } catch (error) {
+    return sendError(500, error.message);
+  }
+};
